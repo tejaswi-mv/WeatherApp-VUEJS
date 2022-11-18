c1f8e0d8413c68a8ac479bb7b7aec3c267baf5d6
Tejaswi <venutejas5@gmail.com>
2022-11-19 02:27:22 +0530

    changes

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+
+    dark: {
+      backgroundColor: "#72ABDC",
+      border: "none",
+      color: "white"
+    }
+
+  },
+  mutations: {},
+  actions: {},
+  modules: {}
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="containter">
+    <div class="switch">
+      <input
+        @input="darkMode = !darkMode, modeChange()"
+        v-model="darkMode"
+        type="checkbox"
+        id="toggle"
+        class="toggle--checkbox"
+      />
+      <label for="toggle" class="toggle--label"></label>
+      <div class="background"></div>
+    </div>
+
+    <div class="header">
+      <h1>Vueather</h1>
+    </div>
+
+    <Search @showCards="showCard = true" @daily="getDaily" @seven="getSeven" />
+
+    <Cards :daily="dailyData" :seven="sevenData" v-if="showCard" />
+  </div>
+</template>
+
+<script>
+import Cards from '../components/cards.vue'
+import Search from '../components/search.vue'
+import { themeConfig } from '../EventBus'
+
+
+export default {
+  name: "Home",
+  components: { Cards, Search },
+  data() {
+    return {
+      sevenData: [],
+      dailyData: [],
+      darkMode: false,
+      showCard: true,
+
+    };
+  },
+  methods: {
+    modeChange() {
+      themeConfig.$emit('dark', this.darkMode)
+    },
+    getDaily(data) {
+      this.dailyData = data
+    },
+
+    getSeven(data) {
+      this.sevenData = data
+    }
+
+  },
+
+}
+
+</script>
+<style>
+
+</style>
+ 
